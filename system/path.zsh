72621579fadca01bf5ad8fad6db247b991dc1498
Jeff Winship <jwin.xal@gmail.com>
2014-04-04 12:50:42 -0600

    xilinx dev branch has some custom path stuff

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,5 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+. set-xilinx-path /opt/Xilinx/Vivado/2013.2
