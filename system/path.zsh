76e15014022c0d673d21aaf28a0886d5e13cc3cc
Jeff Winship <jwin.xal@gmail.com>
2014-04-17 11:29:19 -0600

    removing android paths from master branch

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:/home/winshipj/Development/android-linux-sdk/tools:/home/winshipj/Development/android-linux-sdk/platform-tools:$ZSH/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
