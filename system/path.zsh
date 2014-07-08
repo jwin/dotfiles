daa0f53f72227633ec868e30bf2fbf2d992b9314
Jeff Winship <jwin.xal@gmail.com>
2014-07-07 20:54:04 -0600

    updates to xilinx path

--- system/path.zsh
+++ system/path.zsh
@@ -2,4 +2,4 @@ export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
-. set-xilinx-path /opt/Xilinx/Vivado/2013.2
+. set-xilinx-path /opt/Xilinx/Vivado/2013.4
