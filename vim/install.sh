135cee4c8c09c55ea4587aeae716e87c2dab0b0e
Jeff Winship <jeffrey.winship@lmco.com>
2014-09-03 15:34:40 -0600

    adding tagbar dependancy to install

--- vim/install.sh
+++ vim/install.sh
@@ -10,3 +10,6 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 cd ~/.vim/bundle && \
 git clone http://github.com/tpope/vim-sensible.git
+
+cd ~/.vim/bundle && \
+git clone http://github.com/majutsushi/tagbar.git
